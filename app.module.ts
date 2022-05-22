96e45c74d70e8241a91bb697e4bfa4693e5b235e
Renato <renatohoza99@gmail.com>
2022-05-22 12:08:06 +0300

    Created front page

--- app.module.ts
+++ app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    FrontPageComponent
   ],
   imports: [
     BrowserModule
